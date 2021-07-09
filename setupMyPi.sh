1c37e62c50923c1e6c6dc4a753753c7cc96c748f
Springjunky <thomas.hoeher@gmail.com>
2021-07-09 16:27:13 +0200

    Intermediate state :zombie:
    - refector
    - add iobroker
    - add grafana
    - implement startup-tests

--- setupMyPi.sh
+++ setupMyPi.sh
@@ -6,7 +6,20 @@ if [ $(id -u) -gt 0 ] ;then
     echo "Use sudo $0 "
     exit 1
 fi
+# We need to insall a modern ansible, so we use pip
 
-apt-get install ansible -y
+sudo apt-get install python3 python3-pip -y
+
+ANSIBLE_INSTALLED=$(which ansible)
+
+if [ -z $ANSIBLE_INSTALLED ]; then
+  echo "Install ansible "
+  pip3 install ansible-base
+  echo "Install docker collection for ansible"
+  ansible-galaxy collection install community.docker
+else
+  echo "Ansible installed"
+fi
+
 
 ansible-playbook -v setupPi.yml
